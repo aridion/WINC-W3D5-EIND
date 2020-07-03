e9a0d72238ee603e7401921fa8ddba04c70db48e
Aron Lam <reach@aron.moe>
2020-07-03 17:40:10 +0200

    some css and cleaned up code

--- api-client.js
+++ api-client.js
@@ -2,8 +2,6 @@ const baseURL = `https://wincacademydatabase.firebaseio.com/aron`;
 const jsonEnd = `.json`;
 const ul = document.querySelector("#to-do");
 
-// ul.contentWindow.location.reload(true);
-
 const getList = async () => {
   try {
     const apiAnswer = await fetch(`${baseURL}/tasks${jsonEnd}`, {
@@ -12,13 +10,11 @@ const getList = async () => {
 
     const result = await apiAnswer.json();
 
-    // console.log("Before (the raw result):", result);
     let tasks = Object.keys(result).map((key) => ({
       id: key,
       description: result[key].description,
       done: result[key].done,
     }));
-    // console.log("After the tasks array", tasks);
 
     return tasks;
   } catch (err) {
@@ -35,6 +31,7 @@ const postList = async (input) => {
     const result = await apiAnswer.json();
     console.log(`id for new list item: ${result}`);
     location.reload();
+    // ul.contentWindow.location.reload(true);
   } catch (err) {
     console.log(`this is how you ****ed up in POST: `, err);
   }
@@ -55,3 +52,25 @@ const deleteFromList = async (id) => {
     console.log(`this is how you ****ed up in DELETE: `, err);
   }
 };
+
+// const putList = async (listID, listDesc, listDone) => {
+//   try {
+//     let apiAnswer = null;
+//     if (listDone == true) {
+//       let apiAnswer = await fetch(`${baseURL}/tasks${listID}${jsonEnd}`, {
+//         method: "PUT",
+//         body: `{"description": "${listDesc}", "done": false}`,
+//       });
+//     } else {
+//       let apiAnswer = await fetch(`${baseURL}/tasks${listID}${jsonEnd}`, {
+//         method: "PUT",
+//         body: `{"description": "${listDesc}", "done": true}`,
+//       });
+//     }
+//     const result = await apiAnswer.json();
+//     console.log(`id for new list item: ${result}`);
+//     location.reload();
+//   } catch (err) {
+//     console.log(`this is how you ****ed up in POST: `, err);
+//   }
+// };
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const input = document.querySelector("#add-to-list");
 const button = document.querySelector("#add-button");
 let allTrashIcons = null;
+let allCheckboxes = null;
 
 const clearList = (list) => {
   while (list.firstChild) {
@@ -9,82 +10,60 @@ const clearList = (list) => {
   console.log("after clearlist is complete");
 };
 
-const addFuncTotextInput = () => {
+const displayDomElements = async (list) => {
+  await list.forEach((item) => {
+    const div = ul.appendChild(document.createElement("div"));
+    // div.appendChild(document.createElement("input")).type = "checkbox";
+    div.appendChild(document.createElement("li")).innerText = item.description;
+    div.appendChild(document.createElement("img")).src = "trash.png";
+  });
+};
+
+const addFuncToTextInput = () => {
   button.addEventListener("click", () => {
     postList(input.value);
   });
 };
 
 const addFuncToTrashIcons = async (icons, list) => {
-  await icons.forEach((item) => {
-    item.addEventListener("click", () => {
-      const listId = list[icons.indexOf(item)].id;
+  const currentList = await list;
+  await icons.forEach((icon) => {
+    icon.addEventListener("click", () => {
+      const listId = currentList[icons.indexOf(icon)].id;
       deleteFromList(listId);
       console.log(`using ID: `, listId);
-
-      // const updateDomAfterDel = async () => {};
-      // const reloadDOM = async () => {
-      //   await deleteFromList(listId);
-      //   clearList(ul);
-      //   reloadFunc;
-      //   console.log("after all is complete", reloadFunc);
-
-      // reloadDOM();
-      // };
     });
   });
 };
 
-// ADD TO ELEMENTS TO DOM
+// const addFuncToCheckboxes = async (boxes, list) => {
+//   const currentList = await list;
+//   await boxes.forEach((box) => {
+//     box.addEventListener("click", () => {
+//       const listId = currentList[boxes.indexOf(box)].id;
+//       const listDesc = currentList[boxes.indexOf(box)].description;
+//       const listDone = currentList[boxes.indexOf(box)].done;
+//       deleteFromList(listId, listDesc, listDone);
+//       console.log(`put stuff in: `, currentList);
+//     });
+//   });
+// };
+
+// ADD ALL ELEMENTS AND FUNCTIONALITY TO DOM
 const displayFunctionalList = async (list) => {
   const currentList = await list;
   console.log(currentList);
 
-  const displayDomElements = async () => {
-    await currentList.forEach((item) => {
-      const div = ul.appendChild(document.createElement("div"));
-      div.appendChild(document.createElement("li")).innerText =
-        item.description;
-      div.appendChild(document.createElement("img")).src = "trash.png";
-    });
-  };
-
-  displayDomElements();
+  displayDomElements(currentList);
 
   allTrashIcons = Array.from(document.querySelectorAll("img"));
+  // allCheckboxes = Array.from(document.querySelectorAll("div > input"));
 
   console.log(`in functionalList: `, allTrashIcons);
 
   addFuncToTrashIcons(allTrashIcons, currentList);
+  // addFuncToCheckboxes(allCheckboxes, currentList);
 };
 
 displayFunctionalList(getList());
-addFuncTotextInput();
-
-// console.log(`under functionalList: `, allTrashIcons);
-
-//
-
-// const updateDomAfterDel = async () => {
-//   await deleteFromList(id);
-// };
-// const reloadDOM = async () => {
-//   await updateDomAfterDel();
-//   clearList(ul);
-//   displayDomElements();
-//   console.log("after all is complete");
-// };
-
-// allTrashIcons.forEach(item => {
-//   deleteFunct
-// Array.from.liElements.forEach((element) => {});
-
-// const liElements = document.querySelectorAll("li");
-
-// console.log(liElements);
-
-// const deleteFunct = button.addEventListener("click", (id) => {
-//   deleteList(id);
-//   clearList(ul)
-//   getList();
-// });
+addFuncToTextInput();
